45b87c6da919c8c3579226ef7f963d1eaad12fc3
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-17 12:54:55 +0300

    can change colors of background

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 window.addEventListener('load', function(){
     const button=document.getElementById("btn")
-    const colors=["red","green","blue"]
+    const colors=["red","green","blue","yellow"]
+    const feature=document.getElementById("feature")
+    const name=document.getElementById("name_color")
     
 
     const changeColor=()=>{
-        let color=colors[Math.floor(Math.random()*colors.length)]
-        console.log(color)
+        let color=colors[Math.floor(Math.random()*colors.length)];
+        feature.className=color;
+        name.innerHTML=`Background Color : ${color}`
     }
 
 
