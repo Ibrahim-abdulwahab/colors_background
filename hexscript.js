ac8c0a73d518961c18b3eef6c911015aa2643a1d
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-10-14 17:10:54 +0300

    same but for hex page

--- hexscript.js
+++ hexscript.js
@@ -3,23 +3,12 @@ window.addEventListener('load', function(){
     const colors=["#9287AA","#129E96","#9D86FE"]
     const feature=document.getElementById("feature")
     const name=document.getElementById("name_color")
-    let actualcolor="";
     
 
     const changeColor=()=>{
         let color=colors[Math.floor(Math.random()*colors.length)];
-        if (color=="#9287AA"){
-            actualcolor="a"
-        }
-        if (color=="#129E96"){
-            actualcolor="b"
-        }
-        if (color=="#9D86FE"){
-            actualcolor="c"
-        }
-        feature.className="feature " + actualcolor;
+        feature.style.backgroundColor= color;
         name.innerHTML=`Background Color : ${color}`
-        console.log(actualcolor)
     }
 
 
