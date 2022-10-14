df19dfde58f2f0c877c9429ceef85880ef2c492a
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-10-14 17:27:06 +0300

    the hex page can now show an infinite amount of colors

--- hexscript.js
+++ hexscript.js
@@ -1,12 +1,16 @@
 window.addEventListener('load', function(){
     const button=document.getElementById("btn")
-    const colors=["#9287AA","#129E96","#9D86FE"]
+    const hexes=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
     const feature=document.getElementById("feature")
     const name=document.getElementById("name_color")
-    
 
     const changeColor=()=>{
-        let color=colors[Math.floor(Math.random()*colors.length)];
+        let color= "#"
+        for (let i=0 ; i<6 ; i++){
+            let hex=hexes[Math.floor(Math.random()*hexes.length)];
+            color= color + hex;
+        }
+        console.log(color);
         feature.style.backgroundColor= color;
         name.innerHTML=`Background Color : ${color}`
     }
